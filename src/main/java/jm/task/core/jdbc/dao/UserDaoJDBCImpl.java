6d00b756ded6a2f92209c3c503643873faa5cf36
DmitriyShidlouskij <xubabyb@gmail.com>
2022-03-31 16:36:52 +0300

    disable autocommit connection

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private final Connection connection = Util.getConnection();
 
     public UserDaoJDBCImpl() {
 
@@ -16,6 +15,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     //DDL запрос подходит для использования интерфейса Statement
     public void createUsersTable() {
+        Connection connection = Util.getConnection();
         String query = "CREATE TABLE IF NOT EXISTS users " +
                 "( id BIGINT PRIMARY KEY AUTO_INCREMENT,name VARCHAR (30),lastname VARCHAR (30),age TINYINT)";
         try (Statement statement = connection.createStatement()) {
@@ -28,10 +28,17 @@ public class UserDaoJDBCImpl implements UserDao {
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void dropUsersTable() {
+        Connection connection = Util.getConnection();
         String query = "DROP TABLE IF EXISTS users";
         try (Statement statement = connection.createStatement()) {
             statement.execute(query);
@@ -43,10 +50,17 @@ public class UserDaoJDBCImpl implements UserDao {
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        Connection connection = Util.getConnection();
         String query = "INSERT INTO users (name, lastname, age) VALUE (?,?,?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setString(1, name);
@@ -62,10 +76,17 @@ public class UserDaoJDBCImpl implements UserDao {
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void removeUserById(long id) {
+        Connection connection = Util.getConnection();
         String query = "DELETE FROM users WHERE id = ?";
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             /* DELETE - является оператором DML (Data Manipulation Language) - может удалить часть данных
@@ -81,10 +102,17 @@ public class UserDaoJDBCImpl implements UserDao {
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public List<User> getAllUsers() {
+        Connection connection = Util.getConnection();
         List<User> users = new ArrayList<>();
         String query = "SELECT * FROM users";
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
@@ -107,11 +135,18 @@ public class UserDaoJDBCImpl implements UserDao {
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
         return users;
     }
 
     public void cleanUsersTable() {
+        Connection connection = Util.getConnection();
         String query = "TRUNCATE users";
         try (Statement statement = connection.createStatement()) {
             /* TRUNCATE - является оператором DDL (Data Definition Language) - удаляет все данные из таблицы
@@ -124,6 +159,12 @@ public class UserDaoJDBCImpl implements UserDao {
                 connection.rollback();
             } catch (SQLException ex) {
                 ex.printStackTrace();
+            }
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
     }
